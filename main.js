c0a60ab2d08a59814b6786c26a51c8b9675ae705
Pastel Turtle <60445211+beckyeng226@users.noreply.github.com>
2020-12-22 15:54:27 -0500

    First commit arrays created

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 //Message Generator Project
+//Arrays to pull strings from
+const fairytaleCreatures = ['unicorn', 'witch', 'dragon', 'mermaid', 'wizard', 'ogre', 'dwarf', 'troll', 'hobbit'];
+const actions = ['frolick', 'jump', 'fly', 'swim', 'journey', 'play', 'picnic', 'conjure spells'];
+const preferences = ['wants to', 'loves to', 'hates to'];
+const places = ['to a castle', 'in the forest', 'in a lake', 'through caves', 'to a cottage', 'on a bridge', 'in a tree'];
