f1e21162ee59ef8c74f2f524ab6ffd95e7a8ca2f
Pastel Turtle <60445211+beckyeng226@users.noreply.github.com>
2020-12-22 17:09:52 -0500

    Addition of random element generators

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const fairytaleCreatures = ['unicorn', 'witch', 'dragon', 'mermaid', 'wizard', '
 const actions = ['frolick', 'jump', 'fly', 'swim', 'journey', 'play', 'picnic', 'conjure spells'];
 const preferences = ['wants to', 'loves to', 'hates to'];
 const places = ['to a castle', 'in the forest', 'in a lake', 'through caves', 'to a cottage', 'on a bridge', 'in a tree'];
+
+//Choose random element from each array
+let randCreature = fairytaleCreatures[Math.floor(Math.random() * fairytaleCreatures.length)]
+let randActions = actions[Math.floor(Math.random() * actions.length)]
+let randPreferences = preferences[Math.floor(Math.random() * preferences.length)]
+let randPlaces = places[Math.floor(Math.random()* places.length)]
+
+
+//test to Node
+console.log(fairytaleCreatures)
+console.log(randCreature)
+console.log(randActions)
