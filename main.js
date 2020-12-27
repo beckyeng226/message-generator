57f6bdbb4d3d48a3dec4c9723618731ceff5f92d
Pastel Turtle <60445211+beckyeng226@users.noreply.github.com>
2020-12-27 14:38:58 -0500

    Separate arrays into object

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 //Message Generator Project
 //Arrays to pull strings from
-const fairytaleCreatures = ['unicorn', 'witch', 'dragon', 'mermaid', 'wizard', 'ogre', 'dwarf', 'troll', 'hobbit'];
-const actions = ['frolick', 'jump', 'fly', 'swim', 'journey', 'play', 'picnic', 'conjure spells'];
-const preferences = ['wants to', 'loves to', 'hates to'];
-const places = ['to a castle', 'in the forest', 'in a lake', 'through caves', 'to a cottage', 'on a bridge', 'in a tree'];
+const messagePieces = {
+    fairytaleCreatures: ['unicorn', 'witch', 'dragon', 'mermaid', 'wizard', 'ogre', 'dwarf', 'troll', 'hobbit'],
+    actions: ['frolick', 'jump', 'fly', 'swim', 'journey', 'play', 'picnic', 'conjure spells'],
+    preferences: ['wants to', 'loves to', 'hates to'],
+    places: ['to a castle', 'in the forest', 'in a lake', 'through caves', 'to a cottage', 'on a bridge', 'in a tree'],
+}
 
 //Choose random element from each array
+let randArray = Math.floor(Math.random() * array.length)
 let randCreature = fairytaleCreatures[Math.floor(Math.random() * fairytaleCreatures.length)]
 let randActions = actions[Math.floor(Math.random() * actions.length)]
 let randPreferences = preferences[Math.floor(Math.random() * preferences.length)]
 let randPlaces = places[Math.floor(Math.random()* places.length)]
 
 //Create random messages
-console.log(`Every ${randCreature} ${randPreferences} ${randActions} ${randPlaces}.`)
+let fullMessage = `Every ${randCreature} ${randPreferences} ${randActions} ${randPlaces}.`
+console.log(fullMessage);
