b52b0bc13552d6bd79392203f0602acee7ab7675
Pastel Turtle <60445211+beckyeng226@users.noreply.github.com>
2020-12-27 15:37:28 -0500

    Add functions for random and assembly of message

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 //Message Generator Project
 //Arrays to pull strings from
 const messagePieces = {
-    fairytaleCreatures: ['unicorn', 'witch', 'dragon', 'mermaid', 'wizard', 'ogre', 'dwarf', 'troll', 'hobbit'],
-    actions: ['frolick', 'jump', 'fly', 'swim', 'journey', 'play', 'picnic', 'conjure spells'],
-    preferences: ['wants to', 'loves to', 'hates to'],
+    fairytaleCreatures: ['unicorn ', 'witch ', 'dragon ', 'mermaid ', 'wizard ', 'ogre ', 'dwarf ', 'troll ', 'hobbit '],
+    preferences: ['wants to ', 'loves to ', 'hates to '],
+    actions: ['frolick ', 'jump ', 'fly ', 'swim ', 'journey ', 'play ', 'picnic ', 'conjure spells '],
     places: ['to a castle', 'in the forest', 'in a lake', 'through caves', 'to a cottage', 'on a bridge', 'in a tree'],
 }
 
 //Choose random element from each array
-let randArray = Math.floor(Math.random() * array.length)
-let randCreature = fairytaleCreatures[Math.floor(Math.random() * fairytaleCreatures.length)]
-let randActions = actions[Math.floor(Math.random() * actions.length)]
-let randPreferences = preferences[Math.floor(Math.random() * preferences.length)]
-let randPlaces = places[Math.floor(Math.random()* places.length)]
+const randomMessage = (array) => {
+const randArray = array[Math.floor(Math.random() * array.length)]
+    return randArray;
+}
 
-//Create random messages
-let fullMessage = `Every ${randCreature} ${randPreferences} ${randActions} ${randPlaces}.`
-console.log(fullMessage);
+//Assemble the message pieces into the final message
+const assembleMessage = () => {
+    let message = "";
+
+    for (let word in messagePieces) {
+        message += randomMessage(messagePieces[word]);
+    }
+    console.log("Every " + message + ".");
+}
+
+assembleMessage()
+
